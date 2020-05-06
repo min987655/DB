afc036a455d37e71d40ff3891409ef7c0219255e
1com <123@nate.com>
2020-05-06 16:58:40 +0900

    20.05.06_프로젝트01

---
+++ 20.05.06_chat1_1.sql
@@ -0,0 +1,32 @@
+create sequence num increment by 1 start with 1 maxvalue 99999 cycle;
+
+create table UserContent (
+    pryNumber number(5) NOT NULL PRIMARY KEY,
+    idName varchar2(40),
+    password varchar2(40),
+    name varchar2(10),
+    age varchar2(30),
+    email varchar2(100),
+    phoneNumber varchar2(30)
+);
+
+create table Room (
+    rID number(4) NOT NULL PRIMARY KEY,
+    title varchar2(100),
+    rPassword varchar2(30),
+    userCount varchar2(10),
+    masterName varchar2(100),
+    subject varchar2(30),
+    condtionP number(1) default 0
+);
+
+SELECT * FROM usercontent;
+SELECT * FROM room;
+
+alter table UserContent drop column email;
+alter table UserContent drop column age;
+alter table UserContent drop column phonenumber;
+
+alter table room drop column rpassword;
+alter table room drop column subject;
+alter table room drop column condtionp;
